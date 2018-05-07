c0021d9d1ac5f3f79aef0e9ea7e6c729a890001b
awongCM <awong.cm@gmail.com>
2018-05-07 21:08:37 +1000

    added cors plugin

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const router = require('./routes/index');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((err, req, res, next) => {
+// app.use((err, req, res, next) => {
   
-  // CORS - only for local dev purposes!!
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept ');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+//   // CORS - only for local dev purposes!!
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept ');
+//   res.setHeader('Access-Control-Allow-Credentials', true);
+//   next();
+// });
 
+app.use(cors({credentials:true, origin: true}));
+
+const router = require('./routes/index');
 app.use('/api', router);
 
 app.listen(process.env.PORT  || 7000, () => console.log('Server listening on port 7000'));
